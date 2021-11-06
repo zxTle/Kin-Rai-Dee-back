45f855eecbd3b6f23cffbcef3b486df1dca2d2fe
Tle <chutikarn.mo@ku.th>
2021-11-06 10:12:41 +0700

    Update backend

--- src/main/java/th/ac/ku/KinRaiDee/config/MyConfiguration.java
+++ src/main/java/th/ac/ku/KinRaiDee/config/MyConfiguration.java
@@ -14,7 +14,7 @@ public class MyConfiguration {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
             }
         };
     }
